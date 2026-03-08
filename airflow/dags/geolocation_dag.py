9818f4a28772506dadcdbd48cc575763991d7d5b
Denis Morath <denis@denismorath.de>
2026-03-08 14:08:33 +0100

    write to postgres

--- airflow/dags/geolocation_dag.py
+++ airflow/dags/geolocation_dag.py
@@ -2,9 +2,60 @@
 from geopy.geocoders import Nominatim
 import pendulum
 import requests
+import os
+from sqlalchemy import (
+    create_engine, MetaData, Table,
+    Column, Integer, String, Float
+)
 
+from sqlalchemy.dialects.postgresql import insert
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.python import PythonOperator
+
+def write_initial_interesting_places():
+    interesting_places = [
+        {"id": 1, "city": "Deuselbach"},
+        {"id": 2, "city": "Grünstadt"},
+        {"id": 3, "city": "Ludwigshafen"},
+    ]
+   
+
+    # connection URL – adjust user/host/db/password as needed
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")
+
+    engine = create_engine(DATABASE_URL)
+    metadata = MetaData()
+
+    places = Table(
+        "places",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("city", String),
+        Column("street", String),
+        Column("number", String),
+        Column("postal_code", String),
+        Column("state", String),
+        Column("country", String),
+        Column("latitude", Float),
+        Column("longitude", Float),
+    )
+
+    # create the table in the database if it does not yet exist
+    metadata.create_all(engine)    # issues CREATE TABLE IF NOT EXISTS ...
+
+    stmt = insert(places).values(interesting_places)
+    update_cols = {
+        c.name: getattr(stmt.excluded, c.name)
+        for c in places.c
+        if c.name != "id"
+    }
+    stmt = stmt.on_conflict_do_update(
+        index_elements=["id"],
+        set_=update_cols,
+    )
+
+    with engine.begin() as conn:
+        conn.execute(stmt)
 
 def request_coordinates():
     # calling the Nominatim tool and create Nominatim class
@@ -35,4 +86,5 @@ with DAG(
     schedule="@daily",
     default_args={"retries": 2},
 ):
+   op = PythonOperator(task_id="write_initial_interesting_places", python_callable=write_initial_interesting_places)
    op = PythonOperator(task_id="request_coordinates", python_callable=request_coordinates)
--- airflow/test_geolocation_dag.py
+++ airflow/test_geolocation_dag.py
@@ -2,7 +2,9 @@
 
 
 import pytest
-from dags.geolocation_dag import request_coordinates
+import os
+from sqlalchemy import create_engine, select, column, table
+from dags.geolocation_dag import request_coordinates, write_initial_interesting_places
 
 def test_request_coordinates():
     res = request_coordinates()
@@ -10,3 +12,14 @@ def test_request_coordinates():
     assert "longitude" in res
     assert isinstance(res["latitude"], float)
     assert isinstance(res["longitude"], float)
+
+def test_write_initial_interesting_places():
+    write_initial_interesting_places()
+    conn = create_engine(os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")).connect()
+    stmt = select(column('city')).select_from(table('places'))
+    result = conn.execute(stmt).fetchall()
+    cities = [row[0] for row in result]
+    assert "Deuselbach" in cities
+    assert "Grünstadt" in cities
+    assert "Ludwigshafen" in cities
+    assert "non-existing-city" not in cities
